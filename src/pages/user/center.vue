4e4fec46291ba7fe2f4261c1e417df3e55990020
root <447818666@qq.com>
2020-01-11 17:31:01 +0800

    fix

--- src/pages/user/center.vue
+++ src/pages/user/center.vue
@@ -1,69 +1,64 @@
 <template>
 
 
-    <div class="login-wrap">
-        <transition name="slideT">
-            <div class="mainContent" >
+    <div>
 
-                <div class="ms-login" style="overflow: initial">
+        <Card style="width:780px;margin:2em auto;">
+            <p slot="title">
+                用户操作
+            </p>
 
-                    <Tabs style="overflow: initial">
-                        <Tab-pane label="用户操作" name="account" icon="android-person">
+            <Form :model="formItem" :rules="rules" ref="loginForm" :label-width="80" class="demo-ruleForm">
 
-                            <Form :model="formItem" :rules="rules" ref="loginForm" :label-width="80" class="demo-ruleForm">
+                <Form-item prop="phone" label="手机号">
+                    <Input :maxlength="11" placeholder="请输入手机号" v-model="formItem.phone"></Input>
+                </Form-item>
 
-                                <Form-item prop="phone" label="手机号">
-                                    <Input  :maxlength="11" placeholder="请输入手机号" v-model="formItem.phone"></Input>
-                                </Form-item>
+                <Form-item prop="name" label="姓名">
+                    <Input placeholder="请输入姓名" v-model="formItem.name"></Input>
+                </Form-item>
 
-                                <Form-item prop="name" label="姓名">
-                                    <Input placeholder="请输入姓名" v-model="formItem.name"></Input>
-                                </Form-item>
+                <FormItem label="性别" prop="gender">
+                    <RadioGroup v-model="formItem.gender">
+                        <Radio label="male">男</Radio>
+                        <Radio label="female">女</Radio>
+                    </RadioGroup>
+                </FormItem>
 
-                                <FormItem label="性别" prop="gender">
-                                    <RadioGroup v-model="formItem.gender">
-                                        <Radio label="male" >男</Radio>
-                                        <Radio label="female" >女</Radio>
-                                    </RadioGroup>
-                                </FormItem>
+                <Form-item prop="email" label="电子邮箱">
+                    <Input :maxlength="30" placeholder="请输入电子邮箱" v-model="formItem.email"></Input>
+                </Form-item>
 
-                                <Form-item prop="email" label="电子邮箱">
-                                    <Input  :maxlength="30" placeholder="请输入电子邮箱" v-model="formItem.email"></Input>
-                                </Form-item>
+                <FormItem label="出生日期" prop="birthday">
+                    <DatePicker type="date" placeholder="请选择出生日期" v-model="formItem.birthday"
+                                placement="top"></DatePicker>
+                </FormItem>
 
-                                <FormItem label="出生日期" prop="birthday" >
-                                    <DatePicker type="date" placeholder="请选择出生日期" v-model="formItem.birthday" placement="bottom"></DatePicker>
-                                </FormItem>
+            </Form>
 
-                            </Form>
-
-                        </Tab-pane>
-                    </Tabs>
-
-                    <div class="login-btn">
-                        <Button type="primary" @click="operate">确定</Button>
-                    </div>
-                    <div class="signup-btn">
-                        <a href="javascript:" @click="back">返回</a>
-                    </div>
-                    <!--<Spin size="large" fix ></Spin>-->
-                </div>
+            <div style="margin-top: 2em;text-align: center">
+                <Button type="primary" @click="operate">修改</Button>
             </div>
-        </transition>
+
+
+        </Card>
+
+
     </div>
+
 
 </template>
 
 <script>
-    const md5=require('md5')
+    const md5 = require('md5')
     import {Util} from '../../assets/js/Util'
+
     export default {
         data() {
             return {
-                isEdit:this.$route.query.opType==='edit',
-                user_id:this.$route.query.user_id,
-                formItem: {
-                },
+                isEdit: this.$route.query.opType === 'edit',
+                user_id: this.$route.query.user_id,
+                formItem: {},
                 rules: {
                     phone: [
                         {required: true, message: "手机号不能为空", trigger: "blur"},
@@ -73,7 +68,7 @@
                         {required: true, message: "性别不能为空", trigger: "change"}
                     ],
                     birthday: [
-                        {required: true, type:"date",message: "出生日期不能为空", trigger: "change"}
+                        {required: true, type: "date", message: "出生日期不能为空", trigger: "change"}
                     ],
                     email: [
                         {required: true, message: "电子邮箱不能为空", trigger: "blur"}
@@ -90,8 +85,7 @@
 
             this.http.post('appoint_wx/user/getById').then((data) => {
 
-                this.formItem=data;
-
+                this.formItem = data;
 
 
             }).catch(err => {
@@ -99,7 +93,7 @@
             })
         },
         methods: {
-            back(){
+            back() {
                 this.$router.push('/user/list')
             },
             operate() {
@@ -108,32 +102,22 @@
                     if (valid) {
 
 
-                        let url='user/add';
-                        if(this.isEdit){
-                            url='user/update'
-                        }
+                        let url = 'appoint_wx/user/update'
 
-                        if(!Util.isValidPhone(this.formItem.phone)){
+                        if (!Util.isValidPhone(this.formItem.phone)) {
                             this.$Message.warning("请输入合法的手机号！")
                             return;
                         }
 
-                        if(!Util.isValidEmail(this.formItem.email)){
+                        if (!Util.isValidEmail(this.formItem.email)) {
                             this.$Message.warning("请输入合法的电子邮箱！")
                             return;
                         }
 
-                        // this.formItem.birthday=this.formItem.bi
 
                         this.http.post(url, this.formItem).then((data) => {
 
-                            if(this.isEdit){
-                                this.$Message.success("修改成功！")
-                            }else{
-                                this.$Message.success("新增成功！")
-                            }
-                            this.$router.push('/user/list')
-
+                            this.$Message.success("修改成功！")
 
 
                         }).catch(err => {
@@ -147,69 +131,10 @@
             },
 
 
-
-
         }
     }
 </script>
 
 <style scoped>
-    .login-wrap {
-        position: relative;
-        width: 100%;
-        height: 100vh;
-        background-size: 100% 100%;
-    }
-
-    .mainContent {
-        position: absolute;
-        left: 50%;
-        margin-left: -190px;
-    }
-
-    .ms-title {
-        width: 100%;
-        margin-bottom: 20px;
-        text-align: center;
-        font-size: 26px;
-        color: rgba(255, 255, 255, 0.8);
-        font-weight: 500;
-        letter-spacing: 4px;
-
-    }
-
-    .ms-login {
-        width: 380px;
-        overflow: hidden;
-        padding: 40px;
-        border-radius: 5px;
-        background: #fff;
-        box-sizing: border-box;
-        position: relative
-    }
-
-    .login-btn {
-        text-align: center;
-    }
-
-    .signup-btn {
-        margin-top: 10px;
-        text-align: center;
-        cursor: pointer;
-    }
-
-    .login-btn button {
-        width: 100%;
-        height: 36px;
-    }
-
-    .slideT-enter-active, .slideT-leave-active {
-        transition: all .6s ease-out;
-    }
-
-    .slideT-enter {
-        opacity: 0;
-        transform: translateY(-30px);
-    }
 
 </style>
